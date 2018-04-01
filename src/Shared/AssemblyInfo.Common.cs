1113b8817a6aa0f3d94b7a66a20b6a896b6e3c90
Fahad Alshaya <fahadabdulaziz@gmail.com>
2018-04-01 21:51:11 +0300

    Update

--- src/Shared/AssemblyInfo.Common.cs
+++ src/Shared/AssemblyInfo.Common.cs
@@ -27,8 +27,8 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("2018.3.12.2")]
+[assembly: AssemblyVersion("1.0.0.6")]
+[assembly: AssemblyFileVersion("2018.4.1.6")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
 
 // Ensure the internals can be tested.
